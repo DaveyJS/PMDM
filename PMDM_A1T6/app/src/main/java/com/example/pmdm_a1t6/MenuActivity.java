845616f66807f39f37a170feb81d21924b29ca59
DaveyJS <dsf0002@alu.medac.es>
2025-12-05 19:16:35 +0100

    PMDM A1T6 Full Task

---
+++ PMDM_A1T6/app/src/main/java/com/example/pmdm_a1t6/MenuActivity.java
@@ -0,0 +1,39 @@
+package com.example.pmdm_a1t6;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MenuActivity extends AppCompatActivity {
+    private TextView mailText;
+    private Button disconnect;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_menu);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.menu), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        mailText = findViewById(R.id.textMail);
+        disconnect = findViewById(R.id.buttonDisconnect);
+
+        Intent intent = getIntent();
+        String mail = intent.getStringExtra("mail");
+        mailText.setText(mail);
+
+        disconnect.setOnClickListener(v -> finish());
+    }
+}
