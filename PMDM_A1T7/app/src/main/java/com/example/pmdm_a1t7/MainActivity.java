d4910f4bcc250ae20ddc2ba099bbf32730873c98
DaveyJS <dsf0002@alu.medac.es>
2025-12-06 14:16:26 +0100

    SoundPool implementation

--- PMDM_A1T7/app/src/main/java/com/example/pmdm_a1t7/MainActivity.java
+++ PMDM_A1T7/app/src/main/java/com/example/pmdm_a1t7/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.pmdm_a1t7;
 
 import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.Button;
@@ -15,11 +16,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer player;
+    SoundPool pool;
     Button play;
     Button pause;
     SeekBar seek;
     TextView current;
     TextView end;
+    Button pool1, pool2, pool3, pool4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +36,23 @@ public class MainActivity extends AppCompatActivity {
         });
 
         player = MediaPlayer.create(this, R.raw.no_time_for_caution);
+        pool = new SoundPool.Builder().setMaxStreams(10).build();
+
+        int hover = pool.load(this, R.raw.menu_focus, 1);
+        int select = pool.load(this, R.raw.menu_accept, 1);
+        int alarm = pool.load(this, R.raw.klaxon1, 1);
+        int portal = pool.load(this, R.raw.wpn_portal_gun_fire_blue_01, 1);
 
         play = findViewById(R.id.btnPlay);
         pause = findViewById(R.id.btnPause);
         seek = findViewById(R.id.seekBar);
         current = findViewById(R.id.textCurrent);
         end = findViewById(R.id.textEnd);
+
+        pool1 = findViewById(R.id.btnPool1);
+        pool2 = findViewById(R.id.btnPool2);
+        pool3 = findViewById(R.id.btnPool3);
+        pool4 = findViewById(R.id.btnPool4);
 
         end.setText(String.format("%02d:%02d",
                 player.getDuration() / 1000 / 60, player.getDuration() / 1000 % 60));
@@ -58,6 +72,11 @@ public class MainActivity extends AppCompatActivity {
 
         play.setOnClickListener(v -> play());
         pause.setOnClickListener(v -> pause());
+
+        pool1.setOnClickListener(v -> pool.play(hover, 1, 1, 1, 0, 1));
+        pool2.setOnClickListener(v -> pool.play(select, 1, 1, 1, 0, 1));
+        pool3.setOnClickListener(v -> pool.play(alarm, 1, 1, 1, 0, 1));
+        pool4.setOnClickListener(v -> pool.play(portal, 1, 1, 1, 0, 1));
 
         seek.setMax(player.getDuration());
         seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
